5417e62f2effcc1f6b79fcd558cff43572767791
karthikkm05 <kmkarthik386@gmail.com>
2022-12-07 09:03:35 +0000

    adding evenodd.sh

---
+++ evenodd.sh
@@ -0,0 +1,20 @@
+#!\bin\bash
+
+
+declare -a even_list=()
+declare -a odd_list=()
+
+
+
+for i in {1 .....100}
+do
+	if [ `expr $i %2`==0 ];
+	then
+		even_list+=($i)
+	else 
+		odd_list+=($i)
+	fi
+done
+echo"list of even numbers :${even_lists[@]}"
+echo"list of odd numbers :${odd_lists[@]}"
+
